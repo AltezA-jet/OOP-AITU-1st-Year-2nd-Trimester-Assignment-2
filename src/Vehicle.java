411a14b5c71eba695c3744b5893a0be1ee8aa187
Alteza13 <antirusher13@gmail.com>
2025-12-28 22:36:21 +0500

    created abstract vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,21 @@
-public class Vehicle {
+public abstract class Vehicle {
+    private String brand;
+    private int year;
+    public Vehicle(String brand,int year){
+        this.brand=brand;
+        this.year=year;
+
+    }
+    abstract void startEngine();
+    abstract void stopEngine();
+    public String getbrand(){
+        return brand;
+    }
+    public int getyear(){
+        return year;
+    }
+    void displayInfo(){
+        System.out.println(brand+" "+year);
+    }
     
 }

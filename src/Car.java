a7200a9f25026d53a30aeba44945d0ebee9add27
Alteza13 <antirusher13@gmail.com>
2025-12-28 22:53:40 +0500

    created subclass car

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,21 @@
-public class Car {
-    
-}
+public class Car extends Vehicle{
+    private int doors;
+    private String fuelType;
+    public int getDoors(){
+        return doors;
+    }
+    public String getFuelType(){
+        return fuelType;
+    }
+
+    public Car(String brand, int year,int doors,String fuelType){
+        super(brand,year);
+        this.doors=doors;    
+        this.fuelType=fuelType;
+    }
+    @Override
+    void startEngine(){System.out.println("Engine Started!");}
+    @Override
+    void stopEngine(){System.out.println("Engine stopped!");}
+
+    }

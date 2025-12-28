44b91b1b95cf9962a1c04aa899d9f1290d7b7549
Alteza13 <antirusher13@gmail.com>
2025-12-28 22:58:51 +0500

    created subclass motorcycle

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,3 +1,15 @@
-public class Motorcycle {
-    
+public class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+    public boolean getHasSidecar(){
+        return hasSidecar;
+    }
+    public Motorcycle(String brand, int year,boolean hasSidecar){
+        super(brand, year);
+        this.hasSidecar=hasSidecar;
+    }
+    @Override
+    void startEngine(){System.out.println("Engine Started!");}
+    @Override
+    void stopEngine(){System.out.println("Engine stopped!");}
+
 }

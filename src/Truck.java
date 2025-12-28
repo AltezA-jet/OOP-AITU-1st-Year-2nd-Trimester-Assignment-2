542d9473d8ebeb1d69572344a06abf68ed2d9da5
Alteza13 <antirusher13@gmail.com>
2025-12-28 23:08:07 +0500

    created subclass truck

--- src/Truck.java
+++ src/Truck.java
@@ -1,3 +1,20 @@
-public class Truck {
-    
+public class Truck extends Vehicle {
+    private double capacity;
+    private int numAxles;
+    public double getCapacity(){
+        return capacity;
+    }
+    public int getNumAxles(){
+        return numAxles;
+    }
+    public Truck(String brand, int year,double capacity,int numAxles){
+        super(brand,year);
+        this.capacity=capacity;
+        this.numAxles=numAxles;
+
+    }
+    @Override
+    void startEngine(){System.out.println("Engine Started!");}
+    @Override
+    void stopEngine(){System.out.println("Engine stopped!");}
 }
